6f723b7c8e3e369e178cdc2921206dc33b27c706
WaShindeiru <franekdata@gmail.com>
2025-06-11 22:13:40 +0200

    final commit

--- Icecube_Dataloader.py
+++ Icecube_Dataloader.py
@@ -3,35 +3,26 @@ import pyarrow.parquet as pq
 import torch
 
 def process_tabular(df_selected):
-    '''
-    Processing input tensor.
-    Input Tensor : 3D Tensor
-    With time, batch_id, charge.
-    '''
-    torch_tensor = torch.tensor(df_selected.astype(int).values)
-    torch_tensor_tf = torch_tensor.unsqueeze(-1)
-    torch_tensor_tf = torch_tensor_tf.type(torch.LongTensor)
-    torch_tensor_tf = torch_tensor_tf.permute(1,2,0)
-    torch_tensor_tf = torch_tensor_tf.float()
-    return(torch_tensor_tf)
+    return (
+        torch.tensor(df_selected.astype(int).values)
+        .unsqueeze(-1)
+        .long()
+        .permute(1, 2, 0)
+        .float()
+    )
 
-class ICECUBE_Dataset(Dataset):
+class IceCube_Dataloader(Dataset):
     def __init__(self, parquetfile, batch_dir, batch_num,mode='Train'):
         self.batch_num = batch_num
         self.train_meta = pq.read_table(parquetfile,filters=[('batch_id','==',self.batch_num)]).to_pandas().reset_index(drop = True)
-        # Appending only HQS
         self.sensor_info_df = pq.read_table(batch_dir+'batch_'+str(batch_num)+'.parquet',filters=[('auxiliary','==',False)]).to_pandas()
         self.sensor_info_df = self.sensor_info_df.drop('auxiliary',axis=1).reset_index()
         self.dataset_mode = mode
+
     def __len__(self):
         return self.sensor_info_df.event_id.nunique()
 
     def __getitem__(self, idx):
-        '''
-        Tensors will be iterated using event_id, in DataLoader.
-        Since Parquet batch files are quite large, parquet files of interest loaded in __init__ function.
-        And then generate input tensors using event_id as an index.
-        '''
         event_id = self.sensor_info_df.event_id.unique()
         sensor_info_df_tmp = self.sensor_info_df
         sensor_info_df_tmp = sensor_info_df_tmp[sensor_info_df_tmp.event_id==event_id[idx]].drop('event_id',axis=1)
@@ -45,16 +36,8 @@ class ICECUBE_Dataset(Dataset):
         return sample
 
 def collate_fn(batch):
-    '''
-    Resize 3D Tensors to the biggest tensor in a single batch.
-    This code would resize all tensors in each batch to the tensor which have largest size (w*h).
-    This collate function is applied for batch training, which requires same size of inputs to be feeded.
-    '''
-
-    # Find the largest width and height in the batch
     max_width = max(dat['input_tensor'].shape[2] for dat in batch)
     max_height = max(dat['input_tensor'].shape[1] for dat in batch)
-    channels = max(dat['input_tensor'].shape[0] for dat in batch)
 
     resized_batch = []
     for dat in batch:
